f26576bc0d69cfa9ebdbc0c641f8bfc7fcb67a31
AnaCarol06 <103433120+AnaCarol06@users.noreply.github.com>
2024-09-03 23:03:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const primeiroNome = "Ana Caroline";
+const sobrenome = "Camargo de Lima";
+const idade = 17;
+const cidadeNatal = "Curitiba";
+const altura = 1.58;
+
+console.log("O meu primeiro nome é: ");
+console.log(primeiroNome);
+console.log("O meu sobrenome é: ");
+console.log(sobrenome);
+console.log("A minha idade é: ");
+console.log(idade);
+console.log("Eu nasci na cidade: ");
+console.log(cidadeNatal);
+console.log("Minha altura é: ");
+console.log(altura);
